ac8863790902228739c43c6b2cb8d4ce82381874
Tatiana <tmelnik03@gmail.com>
2024-04-23 13:53:34 +0300

    Добавлен ввод настроек

--- User.cs
+++ User.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Magic;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace Game
+﻿namespace Magic
 {
     class User
     {
         public static void UserGame()
         {
-            Console.WriteLine("Начинаем новую игру!");
+            Console.WriteLine("Начинаем новую игру!"); 
+
+            UserSettings();
+
             // ЗАДАЧА 1
             // Запрашиваем данные у пользователя о длительности игры и вместимости (cost) армии, передаем их в конструктор Settings
             // Создаём экземпляр Settings и заполняем данные в конструкторе 
@@ -21,6 +15,88 @@ namespace Game
             List<IUnit> army1 = CreateArmy(cost);
             List<IUnit> army2 = CreateArmy(cost);
 
+        }
+
+        public static void UserSettings()
+        {
+            // Ввод стоимость армий пользователем.
+            int cost = SetCost();
+
+            // Ввод пользователем длительности игры.
+            int health = Healht();
+
+            // передаем их в конструктор Settings и реализуем Синглтон 
+        }
+
+        static int Healht()
+        {
+            int health = -1;
+
+            Console.CursorVisible = false;
+
+            string[] menuItems = { "Короткая", "Средняя", "Длинная" };
+            int selectedItemIndex = 0;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Выберите длительность игры:");
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (i == selectedItemIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write(">");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.WriteLine(" " + menuItems[i]);
+
+                    Console.ResetColor();
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedItemIndex = (selectedItemIndex - 1 + menuItems.Length) % menuItems.Length;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        selectedItemIndex = (selectedItemIndex + 1) % menuItems.Length;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        health = ExecuteMenuItem(selectedItemIndex);
+                        break;
+                }
+                if (keyInfo.Key == ConsoleKey.Enter) break;
+            }
+            return health;
+        }
+        static int ExecuteMenuItem(int index)
+        {
+            Console.Clear();
+            Console.WriteLine($"Вы выбрали: {index + 1}");
+            if (index == 0) return 50;
+            else if (index == 1) return 75;
+            else if (index == 2) return 100;
+            return 0;
+        }
+        static int SetCost()
+        {
+            int cost;
+            Console.Write("Введите вместимость (стоимость) одной армии от 1 до 30: ");
+            while (!int.TryParse(Console.ReadLine(), out cost) || cost < 1 || cost > 30)
+            {
+                Console.Write("Неправильный ввод данных. Попробуйте ещё раз: ");
+            }
+            return cost;
         }
 
         static List<IUnit> CreateArmy(int cost)
