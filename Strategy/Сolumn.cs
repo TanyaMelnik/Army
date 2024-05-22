71bf500d1728d1705119b86fad74d0a291f67c18
katenester <nesterenkokm@bk.ru>
2024-05-22 13:39:28 +0300

    Логика удаления юнитов

--- Strategy/Сolumn.cs
+++ Strategy/Сolumn.cs
@@ -7,16 +7,108 @@ using System.Threading.Tasks;
 
 namespace Magic
 {
-    class Сolumn : ITypeConstruction
+    // Колонна
+    class Сolumn : ITypeConstruction, Dead
     {
+        public void DeleteUnit(List<IUnit> army, int numberUnit)
+        {
+            // Смещение юнита вперёд
+            army.RemoveAt(numberUnit);
+        }
+
         public void DoSpecialProperties(List<IUnit> army1, List<IUnit> army2)
         {
-            throw new NotImplementedException();
+            // Счётчик для army1
+            int i = 1;
+            // Счётчик для army2
+            int j = 1;
+            if (army2.Count < 2 && army1.Count < 2)
+            {
+                return;
+            }
+            // Величина армий для проверки 
+            int army1Count = army1.Count;
+            int army2Count = army2.Count;
+            // Добавить проверки + генетик !!!
+            while (i < Math.Max(army1Count, army2Count) && j < Math.Max(army1Count, army2Count))
+            {
+                // Величина армий для проверки 
+                army1Count = army1.Count;
+                army2Count = army2.Count;
+                // Применяем специальное свойство из первой армии 
+                if (i < army1.Count)
+                {
+                    if (army1[i] is LogGetAttack unitTemp1 && unitTemp1.unit is ISpecialProperty unit1)
+                    {
+                        var proxy = new ProxyLogSpecial(unit1);
+                        // Вызов метода DoSpecialProperty через экземпляр ProxyLogSpecial
+                        if (army1.Count > 0 && army2.Count > 0)
+                        {
+                            IUnit newUnit = proxy.DoSpecialProperty(army1, army2, i);
+                            // Если генетик склонировал солдата
+                            if (newUnit != null)
+                            {
+                                // Вставляем нового солдата перед ним
+                                army1.Insert(i, newUnit);
+                                i++;
+                            }
+                            // Если был не генетик => проверяем число второй армии.
+                            else
+                            {
+                                // Если во второй армии убили 
+                                if (army2.Count != army2Count)
+                                {
+                                    j--;
+                                }
+                            }
+                        }
+                    }
+                    i++;
+                }
+                // Применяем специальное свойство из второй армии 
+                if (j < army2.Count)
+                {
+                    if (army2[j] is LogGetAttack unitTemp2 && unitTemp2.unit is ISpecialProperty unit2)
+                    {
+                        var proxy = new ProxyLogSpecial(unit2);
+                        if (army1.Count > 0 && army2.Count > 0)
+                        {
+                            // Вызов метода DoSpecialProperty через экземпляр ProxyLogSpecial
+                            IUnit newUnit2 = proxy.DoSpecialProperty(army2, army1, j);
+                            // Если генетик склонировал солдата
+                            if (newUnit2 != null)
+                            {
+                                // Вставляем нового солдата перед ним
+                                army2.Insert(j, newUnit2);
+                                j++;
+                            }
+                            // Если был не генетик => проверяем число второй армии.
+                            else
+                            {
+                                Update(army1);
+                                // Если во второй армии убили 
+                                if (army1.Count != army1Count)
+                                {
+                                    i--;
+                                }
+                            }
+                        }
+                    }
+                    j++;
+                }
+            }
         }
 
         public void MakeMeleeFight(List<IUnit> army1, List<IUnit> army2)
         {
-            throw new NotImplementedException();
+            ProxyDie proxy = new(new DeadUnit());
+            army2[0].GetHit(army1[0].Attack());
+            if (army2[0].Health() < 0) proxy.DeleteUnit(army2, 0);
+            if (army2.Count > 0)
+            {
+                army1[0].GetHit(army2[0].Attack());
+                if (army1[0].Health() < 0) proxy.DeleteUnit(army1, 0);
+            }
         }
 
         public void Show(List<IUnit> army1, List<IUnit> army2)
