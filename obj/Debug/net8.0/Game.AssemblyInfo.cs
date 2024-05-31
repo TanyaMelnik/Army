cdfe569848d13fb6d8878fcc2af863600bb98cd0
elayzandra <elayzandra@yandex.ru>
2024-05-31 22:24:56 +0300

    коммит что ли

--- obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21355ba39ccbac862215df599fdb9a1aa4f3bf5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
