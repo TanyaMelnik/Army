fb6733d6b7e5131d17595122685a6f03d2c979b8
elayzandra <elayzandra@yandex.ru>
2024-06-01 01:01:22 +0300

    бизнес готов

--- BusinessLogic/Settings.cs
+++ BusinessLogic/Settings.cs
@@ -1,23 +1,44 @@
 ﻿namespace Magic
 {
+    /// <summary>
+    ///  Класс настроек.
+    ///  Строка идентификатора "T:Magic.Settings".
+    /// </summary>
     public class Settings
     {
+        /// <summary>
+        /// Поле, содержащее локкер.
+        /// Строка идентификатора "F:Magic.Settings.locker".
+        /// </summary>
         static readonly object locker = new ();
 
         /// <summary>
         /// Приватное статическое поле instance, которое хранит единственный экземпляр класса.
+        /// Строка идентификатора "F:Magic.Settings.instance".
         /// </summary>
         private static Settings? instance;
+
+        /// <summary>
+        /// Метод, содержащее здоровье.
+        /// Строка идентификатора "M:Magic.Settings.Health".
+        /// </summary>
         public int Health { get; }
+
+        /// <summary>
+        /// Метод, содержащее стоимость.
+        /// Строка идентификатора "M:Magic.Settings.Cost".
+        /// </summary>
         public int Cost { get; }
 
         /// <summary>
-        /// Звук при убийстве unit 
+        /// Поле, содержащее звук убийства.
+        /// Строка идентификатора "F:Magic.Settings.sound".
         /// </summary>
         public static bool sound=true;
 
         /// <summary>
         ///  Приватный конструктор предотвращает создание экземпляров класса извне.
+        /// Строка идентификатора "F:Magic.Settings.Settings(Magic.int, Magic.int)".
         /// </summary>
         /// <param name="helpHealth">Здоровье юнитов.</param>
         /// <param name="helpCost">Сумма для покупки юнитов.</param>
@@ -29,6 +50,7 @@
 
         /// <summary>
         /// Метод получения единственного экземпляра.
+        /// Строка идентификатора "F:Magic.Settings.Settings(Magic.int, Magic.int)".
         /// </summary>
         /// <param name="helpHealth">Здоровье юнитов.</param>
         /// <param name="helpCost">Сумма для покупки юнитов.</param>
@@ -41,7 +63,9 @@
                 {
                     return instance;
                 }
+
                 instance = new Settings(helpHealth, helpCost);
+
                 return instance;
             }
         }
