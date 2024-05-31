71e437ab0946d1b1af50d7f4dc8f2547fb8cec92
elayzandra <elayzandra@yandex.ru>
2024-06-01 02:52:25 +0300

    первая часть стратегии

--- Strategy/Battalion.cs
+++ Strategy/Battalion.cs
@@ -2,16 +2,44 @@
 
 namespace Magic
 {
+    /// <summary>
+    ///  Класс батальона.
+    ///  Строка идентификатора "T:Magic.Battalion".
+    /// </summary> 
     class Battalion : ITypeConstruction, IDead
     {
+        /// <summary>
+        /// Поле первой армии.
+        /// Строка идентификатора "F:Magic.Battalion.army1".
+        /// </summary>
         private List<IUnit> army1;
+
+        /// <summary>
+        /// Поле второй армии.
+        /// Строка идентификатора "F:Magic.Battalion.army2".
+        /// </summary>
         private List<IUnit> army2;
+
+        /// <summary>
+        /// Список первой армии.
+        /// Строка идентификатора "F:Magic.Battalion.Army1".
+        /// </summary>
         public List<IUnit> Army1 { get => army1; set => army1 = value; }
+
+        /// <summary>
+        /// Поле второй армии.
+        /// Строка идентификатора "F:Magic.Battalion.Army2".
+        /// </summary>
         public List<IUnit> Army2 { get => army2; set => army2 = value; }
         public Battalion(List<IUnit> _army1, List<IUnit> _army2)
         {
             (army1, army2) = (_army1, _army2);
         }
+
+        /// <summary>
+        /// Метод, проверяющий декоратоор. 
+        /// Строка идентификатора "M:Magic.Battalion.CheckDecorator".
+        /// </summary>
         public void CheckDecorator()
         {
             for (int i = 1; i < army1.Count - 1; i++)
@@ -20,34 +48,47 @@ namespace Magic
                 if (army1[i] is LogGetAttack lightUnit && lightUnit.unit is LightWarrior)
                 {
                     // А рядом с ним тяжелые солдаты => легкий солдат становится оруженосцем. 
-                    if (i % 3 != 2 && i + 1 <= army1.Count - 1 && army1[i + 1] is LogGetAttack heavyUnit1 && heavyUnit1.unit is HeavyWarrior)
+                    if (i % 3 != 2 && i + 1 <= army1.Count - 1 && army1[i + 1] is LogGetAttack 
+                        heavyUnit1 && heavyUnit1.unit is HeavyWarrior)
                     {
-                        army1[i + 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new HorseDecorator(army1[i + 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
+                        army1[i + 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new 
+                            HorseDecorator(army1[i + 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
                     }
-                    if (i % 3 != 0 && i - 1 >= 0 && army1[i - 1] is LogGetAttack heavyUnit2 && heavyUnit2.unit is HeavyWarrior)
+                    if (i % 3 != 0 && i - 1 >= 0 && army1[i - 1] is LogGetAttack heavyUnit2 && 
+                        heavyUnit2.unit is HeavyWarrior)
                     {
-                        army1[i - 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new HorseDecorator(army1[i - 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
+                        army1[i - 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new 
+                            HorseDecorator(army1[i - 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
                     }
                 }
             }
+
             for (int i = 1; i < army2.Count - 1; i++)
             {
                 // Если нашли легкого солдата.
                 if (army2[i] is LogGetAttack lightUnit && lightUnit.unit is LightWarrior)
                 {
                     // А рядом с ним тяжелые солдаты => легкий солдат становится оруженосцем. 
-                    if (i % 3 != 2 && i + 1 <= army2.Count - 1 && army2[i + 1] is LogGetAttack heavyUnit1 && heavyUnit1.unit is HeavyWarrior)
+                    if (i % 3 != 2 && i + 1 <= army2.Count - 1 && army2[i + 1] is LogGetAttack 
+                        heavyUnit1 && heavyUnit1.unit is HeavyWarrior)
                     {
-                        army2[i + 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new HorseDecorator(army1[i + 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
+                        army2[i + 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new 
+                            HorseDecorator(army1[i + 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
                     }
-                    if (i % 3 != 0 && i - 1 >= 0 && army2[i - 1] is LogGetAttack heavyUnit2 && heavyUnit2.unit is HeavyWarrior)
+                    if (i % 3 != 0 && i - 1 >= 0 && army2[i - 1] is LogGetAttack heavyUnit2 && 
+                        heavyUnit2.unit is HeavyWarrior)
                     {
-                        army2[i - 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new HorseDecorator(army1[i - 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
+                        army2[i - 1] = new HelmDecorator(new ShieldDecorator(new PikeDecorator(new 
+                            HorseDecorator(army1[i - 1], (0, 0)), (0, 0)), (0, 0)), (0, 0));
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Метод, удаляющий юнит. 
+        /// Строка идентификатора "M:Magic.Battalion.DeleteUnit".
+        /// </summary>
         public void DeleteUnit(List<IUnit> army, int numberUnit)
         {
             int i;
@@ -56,27 +97,34 @@ namespace Magic
                 army[i] = army[i+3];
             }
             if (i<army.Count) army.RemoveAt(i);
-
         }
 
+        /// <summary>
+        /// Метод, делающий спец.. 
+        /// Строка идентификатора "M:Magic.Battalion.DoSpecialProperties".
+        /// </summary>
         public void DoSpecialProperties()
         {
             // Счётчик для army1.
             int i = 1;
             // Счётчик для army2.
             int j = 1;
-            while (i < Math.Max(army1.Count, army2.Count) && i < 3 && j < Math.Max(army1.Count, army2.Count) && j < 3)
+            while (i < Math.Max(army1.Count, army2.Count) && i < 3 && j < 
+                Math.Max(army1.Count, army2.Count) && j < 3)
             {
                 // Применяем специальное свойство из первой армии. 
                 if (i < army1.Count && i > army2.Count-1)
                 {
-                    if (army1[i] is LogGetAttack unitTemp1 && unitTemp1.unit is ISpecialProperty unit1)
+                    if (army1[i] is LogGetAttack unitTemp1 && unitTemp1.unit is 
+                        ISpecialProperty unit1)
                     {
                         var proxy = new ProxyLogSpecial(unit1);
                         // Вызов метода DoSpecialProperty через экземпляр ProxyLogSpecial.
                         if (army1.Count > 0 && army2.Count > 0)
                         {
-                            IUnit? newUnit = proxy.DoSpecialPropertyBattalion(army1, army2, i);
+                            IUnit? newUnit = proxy.DoSpecialPropertyBattalion(army1, 
+                                army2, i);
+
                             // Если генетик склонировал солдата
                             if (newUnit != null)
                             {
@@ -93,17 +141,21 @@ namespace Magic
                     }
                     
                 }
+
                 i++;
+
                 // Применяем специальное свойство из второй армии. 
                 if (j < army2.Count && j > army1.Count - 1)
                 {
-                    if (army2[j] is LogGetAttack unitTemp2 && unitTemp2.unit is ISpecialProperty unit2)
+                    if (army2[j] is LogGetAttack unitTemp2 && unitTemp2.unit is 
+                        ISpecialProperty unit2)
                     {
                         var proxy = new ProxyLogSpecial(unit2);
                         if (army1.Count > 0 && army2.Count > 0)
                         {
                             // Вызов метода DoSpecialProperty через экземпляр ProxyLogSpecial.
                             IUnit? newUnit2 = proxy.DoSpecialPropertyBattalion(army2, army1, j);
+
                             // Если генетик склонировал солдата
                             if (newUnit2 != null)
                             {
@@ -119,6 +171,7 @@ namespace Magic
                         }
                     }   
                 }
+
                 j++;
             }
 
@@ -155,7 +208,9 @@ namespace Magic
                     }
                     
                 }
+
                 i++;
+
                 // Применяем специальное свойство из второй армии. 
                 if (j < army2.Count)
                 {
@@ -181,10 +236,15 @@ namespace Magic
                         }
                     }
                 }
+
                 j++;
             }
         }
 
+        /// <summary>
+        /// Метод, начинающий драку. 
+        /// Строка идентификатора "M:Magic.Battalion.MakeMeleeFight".
+        /// </summary>
         public void MakeMeleeFight()
         {
             ProxyDie proxy = new(new Battalion(army1,army2));
@@ -193,32 +253,49 @@ namespace Magic
                 if (i < army1.Count && i < army2.Count)
                 {
                     army2[i].GetHit(army1[i].Attack());
+
                     if (army2[i].Health() < 0) proxy.DeleteUnit(army2, i);
                 }
+
                 if (i < army1.Count && i < army2.Count)
                 {
                     army1[i].GetHit(army2[i].Attack());
+
                     if (army1[i].Health() < 0) proxy.DeleteUnit(army1, i);
                 }
             }
         }
 
+        /// <summary>
+        /// Метод показа. 
+        /// Строка идентификатора "M:Magic.Battalion.Show".
+        /// </summary>
+        /// <returns>Строка.</returns> 
         public string Show()
         {
-           StringBuilder s = new();
+            StringBuilder s = new();
+
             s.Append("Армия 1: \n");
+            
             for (int i = 0; i < army1.Count; i++)
             {
                 s.Append(army1[i]+"\n");
             }
+            
             s.Append("Армия 2: \n");
+            
             for (int i = 0; i < army2.Count; i++)
             {
                 s.Append(army2[i] + "\n");
             }
+            
             return s.ToString();
         }
 
+        /// <summary>
+        /// Метод обновления. 
+        /// Строка идентификатора "M:Magic.Battalion.Update".
+        /// </summary>
         public void Update(List<IUnit> army)
         {
             for (int i = 0; i < army.Count; i++)
--- Strategy/ITypeConstruction.cs
+++ Strategy/ITypeConstruction.cs
@@ -1,23 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Magic
+﻿namespace Magic
 {
+    /// <summary>
+    /// Интерфейс, определение метода ApplyInterlace
+    /// Строка идентификатора "M:Magic.Battalion.CheckDecorator".
+    /// </summary>
     interface ITypeConstruction
     {
         public List<IUnit> Army1 { get; set; }
         public List<IUnit> Army2 { get; set; }
+
         // Ближний бой
         void MakeMeleeFight();
+
         // Специальные свойства
         void DoSpecialProperties();
+
         // Проверка убитых солдат
         void Update(List<IUnit> army);
+
         // Проверка на декораторы
         void CheckDecorator();
+
         // Состав армии 
         string Show();
     }
